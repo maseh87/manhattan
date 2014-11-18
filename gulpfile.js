9569a0f86b8389b6c69219f55a0541f0fd1c652b
Mason <maseh87@gmail.com>
2014-11-17 20:13:52 -0800

    updated gulp files

--- gulpfile.js
+++ gulpfile.js
@@ -7,19 +7,27 @@ var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
 
+
+var bs = require('browser-sync');
+var reload = bs.reload;
+var IF = require('gulp-if');
+var jshint = require('gulp-jshint');
+var stylus = require('gulp-stylus');
+
 var paths = {
-  sass: ['./scss/**/*.scss']
+  sass: ['./scss/**/*.scss'],
+  stylus: ['www/**/*.styl'],
+  js: ['www/app/**/*.js']
 };
 
 gulp.task('default', ['sass']);
 
+// ionic uses SASSS
 gulp.task('sass', function(done) {
   gulp.src('./scss/ionic.app.scss')
     .pipe(sass())
     .pipe(gulp.dest('./www/css/'))
-    .pipe(minifyCss({
-      keepSpecialComments: 0
-    }))
+    .pipe(minifyCss({keepSpecialComments: 0}))
     .pipe(rename({ extname: '.min.css' }))
     .pipe(gulp.dest('./www/css/'))
     .on('end', done);
@@ -31,20 +39,49 @@ gulp.task('watch', function() {
 
 gulp.task('install', ['git-check'], function() {
   return bower.commands.install()
-    .on('log', function(data) {
-      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
-    });
+  .on('log', function(data) {
+    gutil.log('bower', gutil.colors.cyan(data.id), data.message);
+  });
 });
 
 gulp.task('git-check', function(done) {
   if (!sh.which('git')) {
     console.log(
-      '  ' + gutil.colors.red('Git is not installed.'),
-      '\n  Git, the version control system, is required to download Ionic.',
-      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
-      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
+      ' ' + gutil.colors.red('Git is not installed.'),
+      '\n Git, the version control system, is required to download Ionic.',
+      '\n Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
+      '\n Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
     );
     process.exit(1);
   }
   done();
+});
+
+// all mine
+gulp.task('stylus', function() {
+  return gulp.src(['www/app/style.styl'])
+  .pipe(stylus())
+  .pipe(gulp.dest('www/css'))
+  .pipe(reload({ stream: true }));
+});
+
+gulp.task('jshint', function() {
+  return gulp.src(paths.js)
+  .pipe(reload({ stream: true, once: true }))
+  .pipe(jshint())
+  .pipe(jshint.reporter('jshint-stylish'))
+  .pipe(IF(!bs.active, jshint.reporter('fail')));
+});
+
+gulp.task('serve', function() {
+  bs({
+    notify: true,
+    server: {
+    baseDir: 'www'
+    },
+    files: ['www/app/**/*.html', 'www/index.html']
+  });
+
+  gulp.watch(paths.stylus, ['stylus']);
+  gulp.watch(paths.js, ['jshint']);
 });
